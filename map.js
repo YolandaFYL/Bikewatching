cd0b85e266aa4b60e37170bd4b545d09a78005d0
Yolanda Feng <yuf019@ucsd.edu>
2025-02-25 19:24:09 -0800

    s1

--- map.js
+++ map.js
@@ -1,3 +1,14 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
 console.log("Mapbox GL JS Loaded:", mapboxgl);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoieW9sYW5kYS1mZW5nIiwiYSI6ImNtN2xjMGozdTA2YzIyanB2czhoYTh3ZTEifQ.HXlfGvQ65K13Do908pXrdg';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12', 
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18
+});
