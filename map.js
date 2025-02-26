6a1a6790ab600d1c962bdd9351e7b2b0180e30d0
Yolanda Feng <yuf019@ucsd.edu>
2025-02-25 22:01:34 -0800

    fixed

--- map.js
+++ map.js
@@ -2,11 +2,10 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoieW9sYW5kYS1mZW5nIiwiYSI6ImNtN2xjMGozdTA2YzIyanB2czhoYTh3ZTEifQ.HXlfGvQ65K13Do908pXrdg';
-console.log("Mapbox GL JS Loaded:", mapboxgl);
 
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v12', 
+  style: 'mapbox://styles/mapbox/streets-v12',
   center: [-71.09415, 42.36027],
   zoom: 12,
   minZoom: 5,
@@ -14,101 +13,88 @@ const map = new mapboxgl.Map({
 });
 
 const bikeLaneStyle = {
-    'line-color': 'green', 
-    'line-width': 5, 
-    'line-opacity': 0.5
+  'line-color': 'green',
+  'line-width': 5,
+  'line-opacity': 0.5
 };
 
+function getCoords(station) {
+  const point = new mapboxgl.LngLat(+station.lon, +station.lat);
+  const { x, y } = map.project(point);
+  return { cx: x, cy: y };
+}
+
 map.on('load', async () => {
-    map.addSource('boston_route', {
-      type: 'geojson',
-      data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
-    });
-    map.addSource('cambridge_route', {
-      type: 'geojson',
-      data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
-    });
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
+  });
 
-    map.addLayer({
-      id: 'boston-bike-lanes',
-      type: 'line',
-      source: 'boston_route',
-      paint: bikeLaneStyle
-    });
-    map.addLayer({
-      id: 'cambridge-bike-lanes',
-      type: 'line',
-      source: 'cambridge_route',
-      paint: bikeLaneStyle
-    });
+  map.addSource('cambridge_route', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
+  });
 
-    const jsonData = await d3.json('https://dsc106.com/labs/lab07/data/bluebikes-stations.json');
-    console.log('Loaded JSON Data:', jsonData);
+  map.addLayer({
+    id: 'boston-bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: bikeLaneStyle
+  });
 
-    let stations = jsonData.data.stations;
-    console.log('Stations Array:', stations);
+  map.addLayer({
+    id: 'cambridge-bike-lanes',
+    type: 'line',
+    source: 'cambridge_route',
+    paint: bikeLaneStyle
+  });
 
-    const svg = d3.select('#map').select('svg');
+  const jsonData = await d3.json('https://dsc106.com/labs/lab07/data/bluebikes-stations.json');
+  let stations = jsonData.data.stations;
 
-    function getCoords(station) {
-        const point = new mapboxgl.LngLat(+station.lon, +station.lat);
-        const { x, y } = map.project(point);
-        return { cx: x, cy: y };
-    }
+  const trips = await d3.csv('https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv');
+  const departures = d3.rollup(trips, v => v.length, d => d.start_station_id);
+  const arrivals = d3.rollup(trips, v => v.length, d => d.end_station_id);
 
-    const trips = await d3.csv('https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv');
-    console.log('Loaded Trips Data:', trips);
+  stations = stations.map(station => {
+    const id = station.short_name;
+    station.arrivals = arrivals.get(id) ?? 0;
+    station.departures = departures.get(id) ?? 0;
+    station.totalTraffic = station.arrivals + station.departures;
+    return station;
+  });
 
-    const departures = d3.rollup(
-        trips,
-        (v) => v.length,
-        (d) => d.start_station_id
-    );
-    const arrivals = d3.rollup(
-        trips,
-        (v) => v.length,
-        (d) => d.end_station_id
-    );
-    stations = stations.map((station) => {
-        const id = station.short_name;
-        station.arrivals = arrivals.get(id) ?? 0;
-        station.departures = departures.get(id) ?? 0;
-        station.totalTraffic = station.arrivals + station.departures;
-        return station;
-    });
-
-    const radiusScale = d3
-    .scaleSqrt()
-    .domain([0, d3.max(stations, (d) => d.totalTraffic)])
+  const radiusScale = d3.scaleSqrt()
+    .domain([0, d3.max(stations, d => d.totalTraffic)])
     .range([0, 25]);
 
-    const circles = svg.selectAll('circle')
-      .data(stations)
-      .enter()
-      .append('circle')
-      .attr('r', d => radiusScale(d.totalTraffic)) 
-      .attr('fill', 'steelblue')
-      .attr('stroke', 'white') 
-      .attr('stroke-width', 1) 
-      .attr('opacity', 0.8)
-      .style('pointer-events', 'auto')
-      .each(function (d) {
-        d3.select(this)
-          .append('title')
-          .text(`${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`);
+  const svg = d3.select('#map').select('svg');
+
+  const circles = svg.selectAll('circle')
+    .data(stations)
+    .enter()
+    .append('circle')
+    .attr('r', d => radiusScale(d.totalTraffic))
+    .attr('fill', 'steelblue')
+    .attr('stroke', 'white')
+    .attr('stroke-width', 1)
+    .attr('opacity', 0.8)
+    .style('pointer-events', 'auto')
+    .each(function (d) {
+      d3.select(this)
+        .append('title')
+        .text(`${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`);
     });
 
-    function updatePositions() {
-      circles
-        .attr('cx', d => getCoords(d).cx) 
-        .attr('cy', d => getCoords(d).cy);
-    }
-  
-    updatePositions();
-  
-    map.on('move', updatePositions); 
-    map.on('zoom', updatePositions); 
-    map.on('resize', updatePositions); 
-    map.on('moveend', updatePositions);
-});
+  function updatePositions() {
+    circles
+      .attr('cx', d => getCoords(d).cx)
+      .attr('cy', d => getCoords(d).cy);
+  }
 
+  updatePositions();
+  map.on('move', updatePositions);
+  map.on('zoom', updatePositions);
+  map.on('resize', updatePositions);
+  map.on('moveend', updatePositions);
+});
