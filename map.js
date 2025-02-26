3e91bf5814f85b36ba4fcfec775b36b0ff90c6e6
Yolanda Feng <yuf019@ucsd.edu>
2025-02-25 19:41:24 -0800

    s2.2

--- map.js
+++ map.js
@@ -1,7 +1,5 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
-console.log("Mapbox GL JS Loaded:", mapboxgl);
-
 mapboxgl.accessToken = 'pk.eyJ1IjoieW9sYW5kYS1mZW5nIiwiYSI6ImNtN2xjMGozdTA2YzIyanB2czhoYTh3ZTEifQ.HXlfGvQ65K13Do908pXrdg';
 
 const map = new mapboxgl.Map({
@@ -12,3 +10,23 @@ const map = new mapboxgl.Map({
   minZoom: 5,
   maxZoom: 18
 });
+
+map.on('load', async () => { 
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
+      });
+
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+            'line-color': '#32D400', 
+            'line-width': 5, 
+            'line-opacity': 0.6
+        }
+      });
+    
+    
+  });
