8bbff652d8ddf6dba7626d8948b91cb5ec13ca36
Yolanda Feng <yuf019@ucsd.edu>
2025-02-25 20:01:22 -0800

    s3.1

--- map.js
+++ map.js
@@ -1,6 +1,8 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoieW9sYW5kYS1mZW5nIiwiYSI6ImNtN2xjMGozdTA2YzIyanB2czhoYTh3ZTEifQ.HXlfGvQ65K13Do908pXrdg';
+console.log("Mapbox GL JS Loaded:", mapboxgl);
 
 const map = new mapboxgl.Map({
   container: 'map',
@@ -14,10 +16,16 @@ const map = new mapboxgl.Map({
 const bikeLaneStyle = {
     'line-color': '#32D400', 
     'line-width': 5, 
-    'line-opacity': 0.6
-  };
+    'line-opacity': 0.7
+};
+
+function getCoords(station) {
+    const point = new mapboxgl.LngLat(+station.Long, +station.Lat);
+    const { x, y } = map.project(point);
+    return { cx: x, cy: y };
+}
   
-  map.on('load', async () => {
+map.on('load', async () => {
     map.addSource('boston_route', {
       type: 'geojson',
       data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
@@ -33,11 +41,23 @@ const bikeLaneStyle = {
       source: 'boston_route',
       paint: bikeLaneStyle
     });
-  
     map.addLayer({
       id: 'cambridge-bike-lanes',
       type: 'line',
       source: 'cambridge_route',
       paint: bikeLaneStyle
     });
-  });
+
+    let jsonData;
+    try {
+        const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
+        
+        const jsonData = await d3.json(jsonurl);
+        console.log('Loaded JSON Data:', jsonData);
+
+        let stations = jsonData.data.stations;
+        console.log('Stations Array:', stations);
+    } catch (error) {
+        console.error('Error loading JSON:', error);
+    }
+});
